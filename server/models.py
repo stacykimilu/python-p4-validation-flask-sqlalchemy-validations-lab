c23033d0ef5c7cb907852a8a4e35f24a32af2cd6
Stacy <stacykimilu@gmail.com>
2024-07-07 21:41:28 +0300

    validation flask sqlalchemy

--- server/models.py
+++ server/models.py
@@ -12,6 +12,25 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError("Name cannot be empty")
+        
+        # Check for unique name and see if it already exists
+        existing_author = Author.query.filter_by(name=value).first()
+        if existing_author:
+            raise ValueError("Name must be unique")
+        
+        return value
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, value):
+        if len(value) != 10 or not value.isdigit():
+            raise ValueError("Phone number must be exactly 10 digits")
+        return value
+
+  
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,6 +47,29 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('title')
+    def validate_title(self, key, value):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in value for phrase in clickbait_phrases):
+            raise ValueError("Title is not sufficiently clickbait-y")
+        return value
+    @validates('content')
+    def validate_content(self, key, value):
+        if len(value) < 250:
+            raise ValueError("Content must be at least 250 characters long")
+        return value
+
+    @validates('summary')
+    def validate_summary(self, key, value):
+        if len(value) > 250:
+            raise ValueError("Summary cannot be more than 250 characters long")
+        return value
+
+    @validates('category')
+    def validate_category(self, key, value):
+        if value not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("Category must be either Fiction or Non-Fiction")
+        return value
 
 
     def __repr__(self):
